4ed6b93542a0847b1fffee99e8f5b82d046d5bdd
Mikhail Kuznetsov <mmkuznecov2002@gmail.com>
2024-03-02 11:54:35 +0000

    added beard model

--- start_torchserve.sh
+++ start_torchserve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directly define the list of models to serve
-MODELS="gender emotions"
+MODELS="gender emotions beard"
 
 # Path to the model store directory
 MODEL_STORE="deployment/model_store"
