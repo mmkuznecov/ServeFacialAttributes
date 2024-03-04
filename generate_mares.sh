af6cb3f81baa02f19fa49f96234cecf6d47e33cd
Mikhail Kuznetsov <mmkuznecov2002@gmail.com>
2024-03-04 14:43:39 +0000

    completed face detection - updated startup

--- generate_mares.sh
+++ generate_mares.sh
@@ -1,81 +1,3 @@
-# #!/bin/bash
-
-# # Define the base directory for models and where to store MAR files
-# MODEL_DIR="./models"
-# STORE_DIR="./deployment/model_store"
-
-# # Ensure the model store directory exists
-# mkdir -p $STORE_DIR
-
-# ################################### CLASSIFIER MODELS ####################################
-
-# # Define the handler and "model file" path (since we're using a dynamic handler approach)
-# HANDLER="handlers/classifiers_handler/classifier_handler.py"
-# MODEL_FILE="handlers/classifiers_handler/classifier_handler.py" # Dynamic handler acts as the model file in this context
-
-# # Loop through each model directory in the models directory
-# for MODEL_PATH in $MODEL_DIR/*; do
-#     if [ -d "$MODEL_PATH" ]; then
-#         # Extract model name from path
-#         MODEL_NAME=$(basename $MODEL_PATH)
-        
-#         # Assume the weights file is located within a 'weights' subdirectory
-#         WEIGHTS_FILE=$(find $MODEL_PATH/weights -type f -name "*.pth" | head -n 1)
-#         if [ -z "$WEIGHTS_FILE" ]; then
-#             echo "No weights file found for $MODEL_NAME, skipping..."
-#             continue
-#         fi
-        
-#         # Assume the index_to_name file is located directly within the model directory
-#         INDEX_TO_NAME_FILE="$MODEL_PATH/index_to_name.json"
-#         if [ ! -f "$INDEX_TO_NAME_FILE" ]; then
-#             echo "No index_to_name.json file found for $MODEL_NAME, skipping..."
-#             continue
-#         fi
-        
-#         # Command to generate the MAR file using torch-model-archiver
-#         torch-model-archiver --model-name $MODEL_NAME \
-#                              --version 1.0 \
-#                              --model-file $MODEL_FILE \
-#                              --serialized-file $WEIGHTS_FILE \
-#                              --handler $HANDLER \
-#                              --extra-files $INDEX_TO_NAME_FILE,handlers/classifiers_handler/customresnetclassifier.py \
-#                              --export-path $STORE_DIR \
-#                              --force
-
-#         echo "Generated MAR file for $MODEL_NAME at $STORE_DIR/$MODEL_NAME.mar"
-#     fi
-# done
-
-
-# ################################### HEADPOSE MODEL ####################################
-
-# # Set model specific variables
-# HEADPOSE_MODEL_NAME="headpose"
-# HEADPOSE_WEIGHTS_FILE="$MODEL_DIR/headpose/weights/headpose_weights.pth"
-# HEADPOSE_EXTRA_FILES="$MODEL_DIR/headpose/sixdrepnet360.py,$MODEL_DIR/headpose/sixderpnet360_utils.py"
-
-# # Check if the weights file exists
-# if [ ! -f "$HEADPOSE_WEIGHTS_FILE" ]; then
-#     echo "No weights file found for $HEADPOSE_MODEL_NAME, skipping..."
-# else
-#     # Command to generate the MAR file for headpose model
-#     torch-model-archiver --model-name $HEADPOSE_MODEL_NAME \
-#                          --version 1.0 \
-#                          --model-file $HEADPOSE_MODEL_FILE \
-#                          --serialized-file $HEADPOSE_WEIGHTS_FILE \
-#                          --handler $HEADPOSE_HANDLER \
-#                          --extra-files $HEADPOSE_EXTRA_FILES \
-#                          --export-path $STORE_DIR \
-#                          --force
-
-#     echo "Generated MAR file for $HEADPOSE_MODEL_NAME at $STORE_DIR/$HEADPOSE_MODEL_NAME.mar"
-# fi
-
-
-# echo "MAR file generation complete."
-
-
 #!/bin/bash
 
 # Define the base directory for models and where to store MAR files
@@ -123,17 +45,18 @@ done
 # Variables specific to the headpose model
 
 HEADPOSE_MODEL_NAME="headpose"
-HEADPOSE_HANDLER="handlers/headpose_handler"
+HEADPOSE_HANDLER_DIR="handlers/headpose_handler"
+HEADPOSE_HANDLER="handlers/headpose_handler/headposehandler.py"
 HEADPOSE_WEIGHTS_FILE="$MODEL_DIR/headpose/weights/headpose_weights.pth"
-HEADPOSE_EXTRA_FILES="$HEADPOSE_HANDLER/sixdrepnet360.py,$HEADPOSE_HANDLER/sixderpnet360_utils.py"
+HEADPOSE_EXTRA_FILES="$HEADPOSE_HANDLER_DIR/sixdrepnet360.py,$HEADPOSE_HANDLER_DIR/sixderpnet360_utils.py"
 
 if [ -f "$HEADPOSE_WEIGHTS_FILE" ]; then
     # Command to generate the MAR file for headpose model
     torch-model-archiver --model-name $HEADPOSE_MODEL_NAME \
                          --version 1.0 \
-                         --model-file $HEADPOSE_HANDLER/headposehandler.py \
+                         --model-file $HEADPOSE_HANDLER \
                          --serialized-file $HEADPOSE_WEIGHTS_FILE \
-                         --handler $HEADPOSE_HANDLER/headposehandler.py \
+                         --handler $HEADPOSE_HANDLER \
                          --extra-files $HEADPOSE_EXTRA_FILES \
                          --export-path $STORE_DIR \
                          --force
@@ -142,5 +65,26 @@ if [ -f "$HEADPOSE_WEIGHTS_FILE" ]; then
 else
     echo "No weights file found for $HEADPOSE_MODEL_NAME, skipping..."
 fi
+
+################################### FACE DETECTION MODEL ####################################
+
+FACE_DETECTION_MODEL_NAME="face_detection"
+FACE_DETECTION_HANDLER="handlers/face_detection_handler/face_detection_handler.py"
+FACE_DETECTION_WEIGHTS_FILE="$MODEL_DIR/face_detection/weights/yolov8n-face.pt"
+
+if [ -f "$FACE_DETECTION_WEIGHTS_FILE" ]; then
+    torch-model-archiver --model-name $FACE_DETECTION_MODEL_NAME \
+                         --version 1.0 \
+                         --model-file $FACE_DETECTION_HANDLER \
+                         --serialized-file $FACE_DETECTION_WEIGHTS_FILE \
+                         --handler $FACE_DETECTION_HANDLER \
+                         --export-path $STORE_DIR \
+                         --force
+
+    echo "Generated MAR file for $FACE_DETECTION_MODEL_NAME at $STORE_DIR/$FACE_DETECTION_MODEL_NAME.mar"
+else
+    echo "No weights file found for $FACE_DETECTION_MODEL_NAME, skipping..."
+fi
+
 
 echo "MAR file generation complete."
--- start_torchserve.sh
+++ start_torchserve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directly define the list of models to serve
-MODELS="gender emotions beard headpose"
+MODELS="gender emotions beard headpose face_detection"
 
 # Path to the model store directory
 MODEL_STORE="deployment/model_store"
