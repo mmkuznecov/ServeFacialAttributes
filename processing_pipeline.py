7e055f6e2790fa05a16c60a95305da296dc6f9e3
Mikhail Kuznetsov <mmkuznecov2002@gmail.com>
2024-03-07 15:17:25 +0000

    updated processing script

--- processing_pipeline.py
+++ processing_pipeline.py
@@ -8,7 +8,10 @@ async def predict_image_async(session, image_path, model_url):
     with open(image_path, 'rb') as img_file:
         img_binary = img_file.read()
 
-    async with session.post(model_url, data=img_binary, headers={'Content-Type': 'application/octet-stream'}) as response:
+    async with session.post(
+            model_url,
+            data=img_binary,
+            headers={'Content-Type': 'application/octet-stream'}) as response:
         if response.status == 200:
             response_text = await response.text()
             try:
@@ -23,7 +26,10 @@ async def predict_image_async(session, image_path, model_url):
 async def predict_images_async(image_path, service_url, models):
     results = {}
     async with aiohttp.ClientSession() as session:
-        tasks = [predict_image_async(session, image_path, f"{service_url}{model}") for model in models]
+        tasks = [
+            predict_image_async(session, image_path, f"{service_url}{model}")
+            for model in models
+        ]
         predictions = await asyncio.gather(*tasks)
         results = dict(zip(models, predictions))
     return results
@@ -34,9 +40,16 @@ def predict_image(image_path, service_url, models):
 
 
 if __name__ == "__main__":
-    image_path = 'test_images/baldboi.jpg'
+
     service_url = 'http://localhost:8080/predictions/'
-    models = os.listdir('models')
-    predictions = predict_image(image_path, service_url, models)
-    for model, result in predictions.items():
-        print(f"Model: {model}, Prediction: {result}")
+    images_dir = 'test_images'
+    images = [
+        os.path.join(images_dir, img_path)
+        for img_path in os.listdir(images_dir)
+    ]
+    for image_path in images:
+        print(f'Results for image {image_path}')
+        models = os.listdir('models')
+        predictions = predict_image(image_path, service_url, models)
+        for model, result in predictions.items():
+            print(f"Model: {model}, Prediction: {result}")
