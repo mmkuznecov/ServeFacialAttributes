66b0f61edc4d61849066fc9c15f1f58b820f6853
Mikhail Kuznetsov <mmkuznecov2002@gmail.com>
2024-05-02 14:47:39 +0000

    updated skincolor calculator + minor updates

--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import Mock
+import base64
 
 
 @pytest.fixture
@@ -14,12 +15,18 @@ def mock_context(request):
         if "model_dir" in request.param:
             context.system_properties["model_dir"] = request.param["model_dir"]
         if "serialized_file" in request.param:
-            context.manifest["model"] = {"serializedFile": request.param["serialized_file"]}
+            context.manifest["model"] = {
+                "serializedFile": request.param["serialized_file"]
+            }
 
     return context
 
 
-def load_image_as_request_input(image_path):
+def load_image_as_request_input(image_path, encode_base64=False):
     with open(image_path, "rb") as img_file:
-        image_bytes = img_file.read()
-    return [{"body": image_bytes}]
+        data = img_file.read()
+
+    if encode_base64:
+        data = base64.b64encode(data).decode("utf-8")
+
+    return [{"body": data}]
